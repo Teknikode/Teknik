356a9d1ee29eafda6851d26a890a85f5d8302595
Uncled1023 <admin@teknik.io>
2015-07-16 21:06:27 -0700

    Configured remote deployment, changed framework to 4.5.2 to work on IIS.

--- Teknik/Global.asax.cs
+++ Teknik/Global.asax.cs
@@ -19,8 +19,6 @@ namespace Teknik
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            Database.SetInitializer(new SampleData());
         }
     }
 }
