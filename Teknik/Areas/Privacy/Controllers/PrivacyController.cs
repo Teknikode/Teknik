ed7670a119238c87638bcbd04dad1bc5b8a88f33
Uncled1023 <admin@teknik.io>
2015-11-24 12:47:14 -0800

    Added missing privacy controller

---
+++ Teknik/Areas/Privacy/Controllers/PrivacyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Teknik.Controllers;
+
+namespace Teknik.Areas.Privacy.Controllers
+{
+    public class PrivacyController : DefaultController
+    {
+        // GET: Privacy/Privacy
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            ViewBag.Title = Config.Title + " - Privacy";
+            ViewBag.Message = "Teknik privacy policy.";
+
+            return View();
+        }
+    }
+}
