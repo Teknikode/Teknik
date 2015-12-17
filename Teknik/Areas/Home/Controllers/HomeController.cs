702ee986001a2a1f42a22ccd9b73c0d93b56a19b
Uncled1023 <admin@teknik.io>
2015-12-17 15:40:17 -0800

    Added viewmodel and news/podcast displaying to home view

--- Teknik/Areas/Home/Controllers/HomeController.cs
+++ Teknik/Areas/Home/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Teknik.Areas.Blog.Models;
+using Teknik.Areas.Home.ViewModels;
 using Teknik.Controllers;
+using Teknik.Helpers;
 using Teknik.Models;
 
 namespace Teknik.Areas.Home.Controllers
@@ -17,13 +19,26 @@ namespace Teknik.Areas.Home.Controllers
         [AllowAnonymous]
         public ActionResult Index()
         {
+            HomeViewModel model = new HomeViewModel();
+            // Grab the latest site blog posts
+            List<Post> lastSite = new List<Post>();
+            var foundSite = db.Posts.Include("Blog").Include("Blog.User").OrderBy(post => post.DatePosted).Where(p => p.Published && p.BlogId == Constants.SERVERBLOGID).Take(10);
+            if (foundSite != null)
+                lastSite = foundSite.ToList();
+            // Grab the latest user blog posts
             List<Post> lastPosts = new List<Post>();
-            var found = db.Posts.Include("Blog").Include("Blog.User").OrderBy(post => post.DatePosted).Where(p => p.Published).Take(10);
-            if (found != null)
-                lastPosts = found.ToList();
+            var foundPosts = db.Posts.Include("Blog").Include("Blog.User").OrderBy(post => post.DatePosted).Where(p => p.Published).Take(10);
+            if (foundPosts != null)
+                lastPosts = foundPosts.ToList();
+            // Grab the latest podcasts
+            List<Post> lastPods = new List<Post>();
+
+            model.SitePosts = lastSite;
+            model.Podcasts = lastPods;
+            model.BlogPosts = lastPods;
 
             ViewBag.Title = Config.Title;
-            return View(lastPosts);
+            return View(model);
         }
     }
 }
---
+++ Teknik/Areas/Home/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teknik.Areas.Blog.Models;
+
+namespace Teknik.Areas.Home.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Post> SitePosts { get; set; }
+        public List<Post> Podcasts { get; set; }
+        public List<Post> BlogPosts { get; set; }
+    }
+}
