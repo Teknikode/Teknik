d05ffc59a558fa57a31ce9b1e6deab622f131914
Uncled1023 <admin@teknik.io>
2015-12-17 15:22:33 -0800

    Fixed links and hiding non-published articles in main view

--- Teknik/Areas/Home/Controllers/HomeController.cs
+++ Teknik/Areas/Home/Controllers/HomeController.cs
@@ -18,14 +18,9 @@ namespace Teknik.Areas.Home.Controllers
         public ActionResult Index()
         {
             List<Post> lastPosts = new List<Post>();
-            if (db.Posts.Count() > 10)
-            {
-                lastPosts = db.Posts.Include("Blog").Include("Blog.User").OrderBy(post => post.DatePosted).Take(10).ToList();
-            }
-            else if (db.Posts.Any())
-            {
-                lastPosts = db.Posts.Include("Blog").Include("Blog.User").OrderBy(post => post.DatePosted).ToList();
-            }
+            var found = db.Posts.Include("Blog").Include("Blog.User").OrderBy(post => post.DatePosted).Where(p => p.Published).Take(10);
+            if (found != null)
+                lastPosts = found.ToList();
 
             ViewBag.Title = Config.Title;
             return View(lastPosts);
