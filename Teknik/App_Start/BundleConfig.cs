aa7a70ab83ba5e7efca09035693f2415059ebf0e
Uncled1023 <admin@teknik.io>
2015-12-20 13:50:12 -0800

    Fixed missing font. Added config to optimize bundles.

--- Teknik/App_Start/BundleConfig.cs
+++ Teknik/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace Teknik
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
