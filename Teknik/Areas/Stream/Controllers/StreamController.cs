97284560be2ab232f94c15c9ddf80d5191af462e
Uncled1023 <admin@teknik.io>
2016-05-12 00:11:05 -0700

    Added canary to transparency

--- Teknik/Areas/Stream/Controllers/StreamController.cs
+++ Teknik/Areas/Stream/Controllers/StreamController.cs
@@ -24,46 +24,30 @@ namespace Teknik.Areas.Stream.Controllers
         [AllowAnonymous]
         public ActionResult ViewStream(int id)
         {
-            if (Config.StreamConfig.Enabled)
+            try
             {
-                if (id > 0 && id <= Config.StreamConfig.Sources.Count)
+                if (Config.StreamConfig.Enabled)
                 {
-                    // ID is valid, so let's get the stream
-                    string source = Config.StreamConfig.Sources[id - 1];
-
-                    System.IO.Stream stream = null;
-
-                    //This controls how many bytes to read at a time and send to the client
-                    int bytesToRead = 10000;
-
-                    // Buffer to read bytes in chunk size specified above
-                    byte[] buffer = new Byte[bytesToRead];
-
-
-                    try
+                    if (id > 0 && id <= Config.StreamConfig.Sources.Count)
                     {
+                        // ID is valid, so let's get the stream
+                        string source = Config.StreamConfig.Sources[id - 1];
                         //Create a WebRequest to get the file
                         HttpWebRequest fileReq = (HttpWebRequest)HttpWebRequest.Create(source);
 
                         //Create a response for this request
                         HttpWebResponse fileResp = (HttpWebResponse)fileReq.GetResponse();
 
-                        if (fileReq.ContentLength > 0)
-                            fileResp.ContentLength = fileReq.ContentLength;
-
-                        //Get the Stream returned from the response
-                        stream = fileResp.GetResponseStream();
-
-                        return File(stream, "image/jpeg");
+                        return File(fileResp.GetResponseStream(), fileResp.ContentType);
                     }
-                    catch (Exception ex)
-                    {
-                        return Redirect(Url.SubRouteUrl("error", "Error.Http404"));
-                    }
+                    return Redirect(Url.SubRouteUrl("error", "Error.Http404"));
                 }
-                return Redirect(Url.SubRouteUrl("error", "Error.Http404"));
+                return Redirect(Url.SubRouteUrl("error", "Error.Http403"));
             }
-            return Redirect(Url.SubRouteUrl("error", "Error.Http403"));
+            catch (Exception ex)
+            {
+                return Redirect(Url.SubRouteUrl("error", "Error.Exception", new { exception = ex }));
+            }
         }
     }
 }
