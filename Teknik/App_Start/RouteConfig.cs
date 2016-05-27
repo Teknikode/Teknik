654f684c788ceecbba0b7d1ff6a584e75fd64cf8
Uncled1023 <admin@teknik.io>
2016-05-27 15:42:51 -0700

    Cleaned up old code and added route ignore for favicon due to browser bug

--- Teknik/App_Start/RouteConfig.cs
+++ Teknik/App_Start/RouteConfig.cs
@@ -15,32 +15,7 @@ namespace Teknik
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            //routes.MapMvcAttributeRoutes();
-            //routes.Add(new SubdomainRoute());
-
-            //Map subdomains to specific style of routing
-            // Development Routing
-            //routes.MapSubdomainRoute(
-            //     "Dev", // Route name
-            //     "dev." + config.Host, // Domain with parameters 
-            //     "{controller}/{action}",    // URL with parameters 
-            //     new { controller = "Dev", action = "Index" }  // Parameter defaults 
-            // );
-            //// Blog Routing
-            //routes.MapSubdomainRoute(
-            //     "Blog", // Route name
-            //     "{controller}." + config.Host, // Domain with parameters 
-            //     "{username}/{id}",    // URL with parameters 
-            //     new { controller = "Blog", action = "Index" }  // Parameter defaults 
-            // );
-
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}",
-            //    defaults: new { controller = "Home", action = "Index" },
-            //    namespaces: new[] { "Teknik.Controllers" }
-            //);
+            routes.IgnoreRoute("{*favicon}", new { favicon = ".*/favicon\\.ico" });
         }
     }
 }
