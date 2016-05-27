06fae90a4c203a642bc4073a0ca63a9c72ef4e5c
Uncled1023 <admin@teknik.io>
2016-05-26 17:06:37 -0700

    Added Do Not Track support for tracking

--- Teknik/Helpers/Tracking.cs
+++ Teknik/Helpers/Tracking.cs
@@ -18,43 +18,47 @@ namespace Teknik.Helpers
             {
                 try
                 {
-                    string sub = request.RequestContext.RouteData.Values["sub"].ToString();
-                    if (string.IsNullOrEmpty(sub))
+                    // Follow Do Not Track
+                    string doNotTrack = request.Headers["DNT"];
+                    if (!string.IsNullOrEmpty(doNotTrack) && doNotTrack != "1")
                     {
-                        sub = request.Url.AbsoluteUri.GetSubdomain();
+                        string sub = request.RequestContext.RouteData.Values["sub"].ToString();
+                        if (string.IsNullOrEmpty(sub))
+                        {
+                            sub = request.Url.AbsoluteUri.GetSubdomain();
+                        }
+                        if (config.DevEnvironment)
+                        {
+                            sub = "dev - " + sub;
+                        }
+
+                        PiwikTracker.URL = config.PiwikConfig.Url;
+                        PiwikTracker tracker = new PiwikTracker(config.PiwikConfig.SiteId);
+
+                        // Get Request Info
+                        string ipAddress = request.ClientIPFromRequest(true);
+                        tracker.setIp(ipAddress);
+                        tracker.setTokenAuth(config.PiwikConfig.TokenAuth);
+                        tracker.setUrl(request.Url.ToString());
+
+                        tracker.setUserAgent(request.UserAgent);
+
+                        // Get browser info
+                        tracker.setResolution(request.Browser.ScreenPixelsWidth, request.Browser.ScreenPixelsHeight);
+                        tracker.setBrowserHasCookies(request.Browser.Cookies);
+                        if (!string.IsNullOrEmpty(request.Headers["Accept-Language"]))
+                            tracker.setBrowserLanguage(request.Headers["Accept-Language"]);
+                        BrowserPlugins plugins = new BrowserPlugins();
+                        plugins.java = request.Browser.JavaApplets;
+                        tracker.setPlugins(plugins);
+
+                        // Get Referral
+                        if (request.UrlReferrer != null)
+                            tracker.setUrlReferrer(request.UrlReferrer.ToString());
+
+                        // Send the tracking request
+                        tracker.doTrackPageView(string.Format("{0}/{1}", sub, title));
                     }
-                    if (config.DevEnvironment)
-                    {
-                        sub = "dev - " + sub;
-                    }
-
-                    PiwikTracker.URL = config.PiwikConfig.Url;
-                    PiwikTracker tracker = new PiwikTracker(config.PiwikConfig.SiteId);
-                    
-                    // Get Request Info
-                    string ipAddress = request.ClientIPFromRequest(true);
-                    tracker.setIp(ipAddress);
-                    tracker.setTokenAuth(config.PiwikConfig.TokenAuth);
-                    tracker.setUrl(request.Url.ToString());
-
-                    tracker.setUserAgent(request.UserAgent);
-
-                    // Get browser info
-                    tracker.setResolution(request.Browser.ScreenPixelsWidth, request.Browser.ScreenPixelsHeight);
-                    tracker.setBrowserHasCookies(request.Browser.Cookies);
-                    if (!string.IsNullOrEmpty(request.Headers["Accept-Language"]))
-                        tracker.setBrowserLanguage(request.Headers["Accept-Language"]);
-                    BrowserPlugins plugins = new BrowserPlugins();
-                    plugins.java = request.Browser.JavaApplets;
-                    tracker.setPlugins(plugins);
-
-                    // Get Referral
-                    if (request.UrlReferrer != null)
-                        tracker.setUrlReferrer(request.UrlReferrer.ToString());
-
-                    // Send the tracking request
-                    tracker.setRequestTimeout(15);
-                    tracker.doTrackPageView(string.Format("{0}/{1}", sub, title));
                 }
                 catch (Exception ex)
                 {
