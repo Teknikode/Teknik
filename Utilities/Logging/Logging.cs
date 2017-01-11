1920dda2af5a742add965dacd3c0087d7c73f29c
Uncled1023 <admin@teknik.io>
2017-01-10 16:53:54 -0800

    Added initial logging project

---
+++ Utilities/Logging/Logging.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teknik.Logging
+{
+    public static class Logging
+    {
+        private static Logger m_Logger { get; set; }
+        public static Logger Logger
+        {
+            get
+            {
+                if (m_Logger == null)
+                {
+                    Create();
+                }
+                return m_Logger;
+            }
+        }
+
+        public static void Create()
+        {
+            m_Logger = new Logger();
+        }
+    }
+}
