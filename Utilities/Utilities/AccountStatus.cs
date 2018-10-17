02008e949b0e40299ce7b398551dc58cd7ac9aa4
PradeepTheDev <PradeepTheDev@gmail.com>
2018-10-17 12:18:58 +1100

    Update AccountStatus.cs
    documented the method with comments

--- Utilities/Utilities/AccountStatus.cs
+++ Utilities/Utilities/AccountStatus.cs
@@ -2,6 +2,8 @@ namespace Teknik.Utilities
 {
     public enum AccountStatus
     {
+        //Account status is active or not 
+        //Usage : AccountStatus.Active or AccountStatus.Banned
         Active = 0,
         Banned = 1
     }
